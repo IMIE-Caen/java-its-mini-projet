0d8ac0eb398cad92173a82165d158652963f4577
arthur <arthurgandon@hotmail.fr>
2017-01-20 12:05:14 +0100

    Bonjour

--- src/miniprojet/MiniProjet.java
+++ src/miniprojet/MiniProjet.java
@@ -16,7 +16,7 @@ public class MiniProjet {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("CoucouillèreLesAmis");
+        System.out.println("CoucouLesAmis");
     }
     
 }
